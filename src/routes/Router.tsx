b0475b0661011dac68bc50918853e3f5f9a52091
Luis <luisdesouza100@gmail.com>
2022-09-13 21:07:28 -0300

    feat: create success page

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { DefaultLayout } from "../layouts";
-import { Checkout, Home } from "../pages";
+import { Checkout, Home, Success } from "../pages";
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/History" element={<Checkout />} />
+        <Route path="/Checkout" element={<Checkout />} />
+        <Route path="/Success" element={<Success />} />
       </Route>
     </Routes>
   );
